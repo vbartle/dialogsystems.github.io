bfe7422adf4c3dc36cb4a0c4f8dfeee27a5cc27b
Fang Shuo Deng <dfs.arthur@berkeley.edu>
2019-03-20 01:31:54 -0700

    refactoring function

--- handlers/food/getMenu.js
+++ handlers/food/getMenu.js
@@ -1,11 +1,42 @@
-var http = require('https');
+// WEB SCRAPER STUFF
+var scraper = require('./webscraper');
 
-exports.myMenu = function () {
-    //TODO: make this return actual menu lmao sry i can't figure out the https stuff
-    // also need to merge the two npm packages
-    return " THIS IS TEST MENU IN getMenu.js";
+const id = '#node-177'
+const content = '.content'
+const menu = '.menu_wrap_overall'
 
-    // exoected output 
+const baseUrl = 'https://caldining.berkeley.edu/';
+const today = "menu"
+
+let nameToDiningHall = {
+    "Cafe 3": "Cafe_3",
+    "Crossroads": "Crossroads",
+    "Clark Kerr": "Clark_Kerr_Campus",
+    "Foothill": "Foothill"
+}
+// input: takes in a diningHall and a meal 
+// output: a string that's what alexa should read out
+exports.getMenuByLocationAndMeal = function (diningHall, meal, callback) {
+    let diningHallKey = nameToDiningHall[diningHall]
+    if (diningHallKey == undefined) {
+        callback("...But Sorry, I couldn't find the dining hall you were asking for");
+    } else if (meal != "Breakfast" && meal != "Lunch" && meal != "Dinner" && meal != "Brunch") {
+        callback("...But Sorry, is this for breakfast, brunch, lunch or dinner?");
+    }
+
+    scraper.scrapeMenu(today, function(mealsInfo) {
+        let hallMenu = mealsInfo[diningHallKey]
+        if (hallMenu != undefined) {
+            var menu = hallMenu[meal]
+            if (menu != undefined) {
+                callback(menu)
+            } else {
+                callback("...But Sorry, I don't think that meal is served today");    
+            }
+        } else {
+            callback("...But Sorry, I don't think that dining hall is serving food today");
+        }
+    });
 }
 
 
--- handlers/food/webscraper.js
+++ handlers/food/webscraper.js
@@ -1,57 +1,9 @@
-// EXPRESS STUFF
-const express = require('express')
-const port = process.env.PORT || 8000
-
 // WEB SCRAPER STUFF
 var request = require('request');
 var cheerio = require('cheerio');
 
-const id = '#node-177'
-const content = '.content'
-const menu = '.menu_wrap_overall'
-
-const baseUrl = 'https://caldining.berkeley.edu/';
-const today = "menu"
-const today_plus_one = "menu_day2"
-const today_plus_two = "menu_day3"
-const today_plus_three = "menu_day4"
-
-const app = express()
-
-app.get('/', function (req, res) {
-    return res.status(200).json({
-        "Get today's menu": "/plusZero",
-        "Get tomorrow's menu": "/plusOne",
-        "Get 2 days' later's menu": "/plusTwo",
-        "Get 3 days' later's menu": "/plusThree",
-    })
-});
-
-app.get('/plusZero', function (req, res) {
-    scrapeMenu(today, function(mealsInfo) {
-        return res.status(200).json(mealsInfo);
-    });
-});
-
-app.get('/plusOne', function (req, res) {
-    scrapeMenu(today_plus_one, function(mealsInfo) {
-        return res.status(200).json(mealsInfo);
-    });
-});
-
-app.get('/plusTwo', function (req, res) {
-    scrapeMenu(today_plus_two, function(mealsInfo) {
-        return res.status(200).json(mealsInfo);
-    });
-});
-
-app.get('/plusThree', function (req, res) {
-    scrapeMenu(today_plus_three, function(mealsInfo) {
-        return res.status(200).json(mealsInfo);
-    });
-});
-
-function scrapeMenu(day, callback) {
+// using exports.scrapeMenu to stay consistent with Ingrid's pattern in getMenu.js
+exports.scrapeMenu = function (day, callback) {
     var mealsInfo = {}
     const finalUrl = baseUrl + day + '.php';
 
@@ -109,8 +61,76 @@ function isSection(word) {
     return word.match(/([A-Z]+ )*[A-Z]{2,}/g) != null
 }
 
+// EXPRESS STUFF
+const express = require('express')
+const port = process.env.PORT || 8000
+
+const id = '#node-177'
+const content = '.content'
+const menu = '.menu_wrap_overall'
+
+const baseUrl = 'https://caldining.berkeley.edu/';
+const today = "menu"
+const today_plus_one = "menu_day2"
+const today_plus_two = "menu_day3"
+const today_plus_three = "menu_day4"
+
+
+const app = express()
+
+app.get('/', function (req, res) {
+    return res.status(200).json({
+        "Get today's menu": "/plusZero",
+        "Get tomorrow's menu": "/plusOne",
+        "Get 2 days' later's menu": "/plusTwo",
+        "Get 3 days' later's menu": "/plusThree",
+    })
+});
+
+app.get('/plusZero', function (req, res) {
+    exports.scrapeMenu(today, function(mealsInfo) {
+        return res.status(200).json(mealsInfo);
+    });
+});
+
+app.get('/plusOne', function (req, res) {
+    exports.scrapeMenu(today_plus_one, function(mealsInfo) {
+        return res.status(200).json(mealsInfo);
+    });
+});
+
+app.get('/plusTwo', function (req, res) {
+    exports.scrapeMenu(today_plus_two, function(mealsInfo) {
+        return res.status(200).json(mealsInfo);
+    });
+});
+
+app.get('/plusThree', function (req, res) {
+    exports.scrapeMenu(today_plus_three, function(mealsInfo) {
+        return res.status(200).json(mealsInfo);
+    });
+});
+
+// TESTING
+let nameToDiningHall = {
+    "Cafe 3": "Cafe_3",
+    "Crossroads": "Crossroads",
+    "Clark Kerr": "Clark_Kerr_Campus",
+    "Foothill": "Foothill"
+}
+
+let getMenuByLocationAndMeal = require('./getMenu').getMenuByLocationAndMeal
+
+app.get('/test', function (req, res) {
+    getMenuByLocationAndMeal("Cafe 3", "Dinner", function(res) {
+        console.log(res);
+    });
+});
+
+
 app.listen(port, function() {
     console.log("Started listening on port: " + port)
 });
 
-// contain the method
+
+console.log(nameToDiningHall['x'] == undefined)
